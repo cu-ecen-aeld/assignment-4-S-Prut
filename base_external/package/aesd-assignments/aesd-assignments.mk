86b251429edf15314cf77c719576588d75b46e25
S-Prut <xacah2000@gmail.com>
2026-03-11 13:51:03 +0100

    assignment4-part2 completed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '0b0fff2b92534688dead46c7a8d548201baa6352'
+AESD_ASSIGNMENTS_VERSION = 'b3170a65d28921af43a57a0bfde69d42514e5a9d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
