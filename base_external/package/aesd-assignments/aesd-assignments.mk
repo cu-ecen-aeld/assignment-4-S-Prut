cbc269f22121aac32666ea884666e4cd0c75a29e
S-Prut <xacah2000@gmail.com>
2026-03-15 23:31:28 +0100

    conf path in finder-app.sh fixed in the assiggnment3, new hash-summ

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'b3170a65d28921af43a57a0bfde69d42514e5a9d'
+AESD_ASSIGNMENTS_VERSION = '469bd7d46bdf793de6e0c02808e832898ac8ba7a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,8 +23,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-        $(INSTALL) -m 0755 $(@D)/finder-app/finder*.sh $(TARGET_DIR)/etc/finder-app/
-        $(INSTALL) -m 0755 $(@D)/finder-app/writer* $(TARGET_DIR)/etc/finder-app/
+        $(INSTALL) -m 0755 $(@D)/finder-app/finder*.sh $(TARGET_DIR)/usr/bin/
+        $(INSTALL) -m 0755 $(@D)/finder-app/writer* $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
